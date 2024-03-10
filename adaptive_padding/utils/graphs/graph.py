3f19d7538fae592284f33b01124fc7dd66f860ee
Janael Pinheiro <ajp@cesar.org.br>
2024-03-09 21:46:32 -0300

    ordenar as barras dos gráficos

--- adaptive_padding/utils/graphs/graph.py
+++ adaptive_padding/utils/graphs/graph.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any, Tuple
 
 from seaborn import barplot, color_palette, set_style, scatterplot
 import matplotlib.pyplot as plt
@@ -6,6 +6,14 @@ import matplotlib.pyplot as plt
 set_style("whitegrid")
 
 NUMBER_ALGORITHMS: int = 4
+
+
+def _sort_values(x, y) -> Tuple[List[Any], List[Any]]:
+    zipped = zip(y, x)
+    sorted_values = sorted(zipped, reverse=True)
+    sorted_x = [x for _, x in sorted_values]
+    sorted_y = [y for y, _ in sorted_values]
+    return sorted_x, sorted_y
 
 
 def setup_graph(func):
@@ -27,7 +35,8 @@ def setup_graph(func):
 @setup_graph
 def make_barplot(x, y, x_label: str, y_label: str, filename: str, hue: List[str]):
     palette = color_palette("colorblind", n_colors=len(x) // NUMBER_ALGORITHMS)
-    barplot(x=x, y=y, hue=hue, palette=palette)
+    sorted_x, sorted_y = _sort_values(x, y)
+    barplot(x=sorted_x, y=sorted_y, hue=hue, palette=palette)
 
 
 @setup_graph
